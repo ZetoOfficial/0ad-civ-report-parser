8a8266c68ee1a85e81fc41b540d4f775b50557d9
ZetoOfficial <tpptgs@bk.ru>
2026-05-01 12:02:50 +0500

    tech: pair expansion

---
+++ internal/tech/pair_test.go
@@ -0,0 +1,47 @@
+package tech
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/ZetoOfficial/0ad-civ-report-parser/internal/testutil"
+)
+
+func TestExpandPair_Han(t *testing.T) {
+	testutil.SkipIfNoGameData(t)
+
+	c := NewCatalog(filepath.Join(testutil.GameDataRoot(), "simulation", "data", "technologies"))
+	top, bottom, ok := ExpandPair(c, "pair_unlock_civil_service_han")
+	if !ok {
+		t.Fatal("ExpandPair returned ok=false, want true")
+	}
+	if top.Name != "civil_service_01" {
+		t.Errorf("top.Name = %q, want %q", top.Name, "civil_service_01")
+	}
+	if bottom.Name != "civil_service_02" {
+		t.Errorf("bottom.Name = %q, want %q", bottom.Name, "civil_service_02")
+	}
+}
+
+func TestExpandPair_NotPair(t *testing.T) {
+	testutil.SkipIfNoGameData(t)
+
+	c := NewCatalog(filepath.Join(testutil.GameDataRoot(), "simulation", "data", "technologies"))
+	_, _, ok := ExpandPair(c, "phase_town")
+	if ok {
+		t.Error("ExpandPair returned ok=true for non-pair tech, want false")
+	}
+}
+
+func TestExpandPair_Missing(t *testing.T) {
+	testutil.SkipIfNoGameData(t)
+
+	c := NewCatalog(filepath.Join(testutil.GameDataRoot(), "simulation", "data", "technologies"))
+	top, bottom, ok := ExpandPair(c, "no_such_pair_xxx")
+	if ok {
+		t.Error("ExpandPair returned ok=true for missing tech, want false")
+	}
+	if top != nil || bottom != nil {
+		t.Error("ExpandPair returned non-nil top/bottom for missing tech")
+	}
+}
